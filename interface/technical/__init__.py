6a9fcc1fa424406acb26c2039dea39b608fbb640
Charlie Zhang <sunnypig2002@gmail.com>
2015-06-30 09:04:51 +0800

    Charlie Zhang: add interfaces about party

--- interface/technical/__init__.py
+++ interface/technical/__init__.py
@@ -11,3 +11,13 @@ class ILocation(zope.interface.Interface):
     longitude = zope.interface.Attribute('longitude of location')
 
     latitude = zope.interface.Attribute('latitude of location')
+
+
+class ITimePeriod(zope.interface.Interface):
+    """
+    interface of time period
+    """
+
+    time_start = zope.interface.Attribute('time to start')
+
+    time_last = zope.interface.Attribute('time will last')
